ec05944bde931429626f5adf2e533aa01a59e5c6
VostNord <91051943+VostNord@users.noreply.github.com>
2021-09-27 20:58:18 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";}
+
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
